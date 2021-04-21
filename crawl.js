207cbccb45f6d755affb545d7c63e36524be1538
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-21 14:26:53 +0900

    完成

--- crawl.js
+++ crawl.js
@@ -1,5 +1,9 @@
 const puppeteer = require('puppeteer');
 
+async function sleep(delay) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
 (async () => {
   const options = {
     headless: false,
@@ -8,15 +12,17 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   await page.goto('https://reserve.tokyodisneyresort.jp/');
-  const xpath = '//*[@id="contents"]/h3';
-  const elems = await page.$x(xpath);
-  const jsHandle = await elems[0].getProperty('textContent');
-  const text = await jsHandle.jsonValue();
-  if (text == "Thank you for visiting Tokyo Disney Resort Online Reservations & Tickets."){
-    await wait(1000);
+  for( ; ; ) {
+    const xpath = '//*[@id="contents"]/h3';
+    const elems = await page.$x(xpath);
+    const jsHandle = await elems[0].getProperty('textContent');
+    const text = await jsHandle.jsonValue();
+    if (text != "Thank you for visiting Tokyo Disney Resort Online Reservations & Tickets."){
+      console.log("抜ける！");
+    };
+    await sleep(2000);
     console.log("リロードします");
-    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-  };
+}
 
-  await browser.close();
+  // await browser.close();
 })();
